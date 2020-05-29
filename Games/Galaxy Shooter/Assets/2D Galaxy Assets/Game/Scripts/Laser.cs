506a27a01dc0a717ed4de833f8b0cdcf9c454b46
lunardi01 <adrianolunardi01@gmail.com>
2020-05-29 00:02:02 -0300

    commit Galaxy Shooter

---
+++ Games/Galaxy Shooter/Assets/2D Galaxy Assets/Game/Scripts/Laser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if(transform.position.y >= 5.2f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Games/Galaxy Shooter/Assets/2D Galaxy Assets/Game/Scripts/Player.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    public bool canTripleShoot = false;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
+
+    [SerializeField]
+    private float _fireRate = 0.25f;
+
+    private float _nextFire = 0.0f;
+
+    [SerializeField]
+    private float _speed = 5.0f;
+
+
+    void Start()
+    {
+        //current position = new position
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+
+    void Update()
+    {
+        Movement();
+
+        //if space key is presses
+        //spawn laser at player's position
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))     
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > _nextFire)
+        {
+            if (canTripleShoot == true)
+            {
+                Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+                
+            }
+
+            _nextFire = Time.time + _fireRate;
+        }
+    }
+
+    private void Movement()
+    {
+        //player movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
+
+        //boundary on y
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, transform.position.z);
+        }
+
+        //boundary on x
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, transform.position.z);
+        }
+    }
+}
+
